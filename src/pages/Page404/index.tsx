72af950a59b9c481a05ebee0aa44a33ce66e6fa9
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-12-18 07:16:12 -0300

    update images folder

--- src/pages/Page404/index.tsx
+++ src/pages/Page404/index.tsx
@@ -1,11 +1,12 @@
 import { useNavigate } from "react-router-dom";
 import { Container } from "./style";
+import page404 from "../../assets/page404.png";
 
 export function Page404() {
   const navigate = useNavigate();
   return (
     <Container>
-      <img src="../../../public/page404.png" alt="page404" />
+      <img src={page404} alt="page404" />
       <button onClick={() => navigate("/")}>voltar</button>
     </Container>
   );
