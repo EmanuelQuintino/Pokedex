2005ba6dd001019b7a619979d483249e6278a173
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-12-24 20:56:06 -0300

    add style back button page 404

--- src/pages/Page404/style.ts
+++ src/pages/Page404/style.ts
@@ -6,7 +6,24 @@ export const Container = styled.div`
   height: 100vh;
 
   img {
+    margin-top: 3.2rem;
     width: 56rem;
     box-shadow: 1px 1px 2px 1px #0002;
+  }
+
+  button {
+    background: ${({ theme }) => theme.colors.danger};
+    color: ${({ theme }) => theme.colors.light};
+    font-weight: 700;
+    font-size: 1.2rem;
+    padding: 1.2rem;
+    border-radius: 3.2rem;
+    width: 20%;
+    margin: 1.6rem auto 0;
+
+    &:hover {
+      cursor: pointer;
+      filter: brightness(1.05);
+    }
   }
 `;
