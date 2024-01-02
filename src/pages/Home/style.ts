a20ae04aa871986f6a4e44a1765bd9ff81437378
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-01-02 11:51:32 -0300

    add home responsive

--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -4,38 +4,38 @@ export const Container = styled.div`
   min-height: 100vh;
 
   h1 {
-    margin-top: 2.4rem;
-    font-size: 2.8rem;
+    font-size: 2rem;
     text-align: center;
     color: lightgray;
   }
 
   .gridCards {
     min-height: 100vh;
-    margin-top: 4.2rem;
+    margin-top: 3.2rem;
     display: flex;
     align-items: center;
     justify-content: center;
-    gap: 2.4rem;
+    gap: 1.2rem;
     flex-wrap: wrap;
   }
 
   .paginationComponent {
-    width: 32rem;
-    margin: 3.2rem auto 0;
+    width: 24rem;
+    margin: 2.4rem auto 0;
     padding: 0.6rem 1.2rem;
     display: flex;
     align-items: center;
     justify-content: space-between;
 
     .numberPage {
-      font-size: 1.8rem;
+      font-size: 1.2rem;
       font-weight: 700;
     }
 
     button {
       background: ${({ theme }) => theme.colors.danger};
       color: ${({ theme }) => theme.colors.light};
+      font-size: 1rem;
       font-weight: 700;
       padding: 0.8rem 1.6rem;
       border-radius: 0.8rem;
@@ -48,6 +48,31 @@ export const Container = styled.div`
 
       &:disabled {
         background: lightgray;
+      }
+    }
+  }
+
+  @media (min-width: 786px) {
+    h1 {
+      margin-top: 2.4rem;
+      font-size: 2.8rem;
+    }
+
+    .gridCards {
+      margin-top: 4.2rem;
+      gap: 2.4rem;
+    }
+
+    .paginationComponent {
+      width: 32rem;
+      margin: 3.2rem auto 0;
+
+      .numberPage {
+        font-size: 1.8rem;
+      }
+
+      button {
+        font-size: 1.6rem;
       }
     }
   }
