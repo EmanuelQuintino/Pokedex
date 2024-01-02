fd05d54416bc0095d11cd3d6398bfc674e2c8f3e
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-01-02 12:27:58 -0300

    add search page responsive

--- src/pages/SearchPage/style.ts
+++ src/pages/SearchPage/style.ts
@@ -2,52 +2,32 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   min-height: 100vh;
-  
+
   h1 {
-    margin-top: 2.4rem;
-    font-size: 2.8rem;
+    margin-top: 0.8rem;
+    font-size: 1.2rem;
     color: lightgray;
     text-align: center;
   }
 
   .gridCards {
-    margin-top: 4.2rem;
+    margin-top: 2rem;
     display: flex;
     align-items: center;
     justify-content: center;
-    gap: 2.4rem;
+    gap: 1.2rem;
     flex-wrap: wrap;
   }
 
-  .paginationComponent {
-    width: 32rem;
-    margin: 3.2rem auto 0;
-    padding: 0.6rem 1.2rem;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-
-    .numberPage {
-      font-size: 1.8rem;
-      font-weight: 700;
+  @media (min-width: 786px) {
+    h1 {
+      margin-top: 2.4rem;
+      font-size: 2.8rem;
     }
 
-    button {
-      background: ${({ theme }) => theme.colors.danger};
-      color: ${({ theme }) => theme.colors.light};
-      font-weight: 700;
-      padding: 0.8rem 1.6rem;
-      border-radius: 0.8rem;
-      box-shadow: 1px 1px 2px 0px #0008;
-      text-shadow: 1px 1px 2px #0004;
-
-      &:not(:disabled):hover {
-        filter: brightness(1.05);
-      }
-
-      &:disabled {
-        background: lightgray;
-      }
+    .gridCards {
+      margin-top: 4.2rem;
+      gap: 2.4rem;
     }
   }
 `;
