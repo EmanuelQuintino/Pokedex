fbd4f1567a409945cb4e4c2fb30ba500c5d06eaf
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-01-02 19:10:51 -0300

    add boxDetails

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,7 +1,8 @@
 import { useNavigate, useParams } from "react-router-dom";
-import { PokemonCardDetails } from "../../components/PokemonCardDetails";
 import { useQueryPokemon } from "../../hooks/useQueryPokemon";
 import { Container } from "./style";
+import { CardType } from "../../components/CardType";
+import pokeball from "../../assets/pokeball.png";
 
 type Params = {
   name: string;
@@ -16,17 +17,51 @@ export function Details() {
 
   return (
     <Container>
-      <button onClick={() => navigate(-1)} className="buttonBackPage">
-        &lt; voltar
-      </button>
-
       {isLoading && <span className="feedbackLoading">Loading...</span>}
       {!isLoading && error && <span className="feedbackLoading">Error...</span>}
 
       {data && (
-        <>
-          <PokemonCardDetails pokemon={data} />
-        </>
+        <div className="boxDetails">
+          <button onClick={() => navigate(-1)} className="buttonBackPage">
+            &lt; voltar
+          </button>
+
+          <div className="pokemonImage">
+            <img
+              src={data.sprites.other["official-artwork"].front_default || pokeball}
+              alt={data.name}
+            />
+          </div>
+
+          <div className="boxStatus">
+            <strong>
+              #{data.id} {data.name[0].toUpperCase() + data.name.substring(1)}
+            </strong>
+
+            <div className="sizePokemon">
+              <span>Height: {data.height}0cm</span>
+              <span>Weight: {data.weight}kg</span>
+            </div>
+
+            <div className="boxTypes">
+              {data.types.map((type) => {
+                return <CardType key={type.type.name} type={type.type.name} size={16} />;
+              })}
+            </div>
+          </div>
+
+          <div className="boxStats">
+            {data.stats?.map((status) => {
+              return (
+                <div key={status.stat.name} className="stats">
+                  <span className="statsName">{status.stat.name}</span>
+                  <progress max={200} value={status.base_stat} />
+                  <span className="statsValue">{status.base_stat}</span>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       )}
     </Container>
   );
--- src/pages/Details/style.ts
+++ src/pages/Details/style.ts
@@ -1,23 +1,178 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  .buttonBackPage {
-    font-size: 1.4rem;
-    font-weight: 500;
-    position: absolute;
-    top: 210px;
-    left: 32px;
+  .boxDetails {
+    background: ${({ theme }) => theme.colors.light};
+    padding: 2.4rem;
+    border-radius: 0.8rem;
+    box-shadow: 1px 1px 2px 1px #0002;
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    flex-direction: column;
+    position: relative;
 
-    button:hover {
-      text-decoration: underline;
+    .buttonBackPage {
+      font-size: 1.4rem;
+      font-weight: 500;
+      position: absolute;
+      top: 12px;
+      left: 16px;
+
+      button:hover {
+        text-decoration: underline;
+      }
+    }
+
+    .pokemonImage {
+      width: 18rem;
+      border-radius: 100%;
+      background: #ddd8;
+      box-shadow: 1px 1px 2px 1px #0002;
+      margin-top: 1.6rem;
+    }
+
+    .pokemonImage img {
+      filter: drop-shadow(1px 1px 1px #000b);
+      width: 100%;
+    }
+
+    .boxStatus {
+      text-align: center;
+      margin-top: 2.4rem;
+    }
+
+    strong {
+      font-size: 2rem;
+    }
+
+    .sizePokemon {
+      margin-top: 0.8rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: 2.4rem;
+
+      span {
+        font-size: 1.6rem;
+        color: gray;
+      }
+    }
+
+    .boxTypes {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: 0.8rem;
+      margin-top: 2.4rem;
+    }
+
+    .boxStats {
+      margin-top: 2.8rem;
+    }
+
+    .stats {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+
+    .statsName {
+      font-size: 1.4rem;
+      width: 11rem;
+      margin-left: auto;
+    }
+
+    progress {
+      height: 0.6rem;
+
+      &::-webkit-progress-value {
+        background: ${({ theme }) => theme.colors.primary};
+        border-radius: 8px;
+      }
+
+      &::-moz-progress-value {
+        background: ${({ theme }) => theme.colors.primary};
+        border-radius: 8px;
+      }
+
+      &::-webkit-progress-bar {
+        background: lightgray;
+        border-radius: 8px;
+      }
+
+      &::-moz-progress-bar {
+        background: lightgray;
+        border-radius: 8px;
+      }
+    }
+
+    .statsValue {
+      font-size: 1.4rem;
+      width: 2.8rem;
+      text-align: right;
     }
   }
 
   @media (min-width: 786px) {
-    .buttonBackPage {
-      font-size: 1.6rem;
-      top: 210px;
-      left: 52px;
+    .boxDetails {
+      flex-direction: row;
+
+      .buttonBackPage {
+        font-size: 1.6rem;
+        top: 22px;
+        left: 28px;
+      }
+
+      .pokemonImage {
+        width: 28rem;
+        margin-top: 2.4rem;
+      }
+
+      .pokemonImage img {
+        filter: drop-shadow(1px 1px 1px #000b);
+        width: 100%;
+      }
+
+      .boxStatus {
+        text-align: left;
+      }
+
+      strong {
+        font-size: 3.2rem;
+      }
+
+      .sizePokemon {
+        justify-content: start;
+
+        span {
+          font-size: 1.8rem;
+        }
+      }
+
+      .boxTypes {
+        justify-content: start;
+        gap: 1.6rem;
+        margin-top: 2.4rem;
+      }
+
+      .stats {
+        margin-top: 0.2rem;
+      }
+
+      .statsName {
+        font-size: 1.8rem;
+        width: 14rem;
+      }
+
+      progress {
+        height: 0.8rem;
+      }
+
+      .statsValue {
+        font-size: 1.8rem;
+        width: 3.6rem;
+      }
     }
   }
 `;
